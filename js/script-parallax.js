fb8da365562c64a26dd6fa65d5105a2a7ebf4015
Johan VAN DER SLOOTEN <johan.van_der_slooten@devinci.fr>
2018-10-16 09:29:51 +1100

    index

---
+++ js/script-parallax.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var $window = $(window);
+    
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this);
+        
+        $(window).scroll(function(){
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+            var coords = '50%' + yPos + 'px';
+            $bgobj.css({ backgroundPosition: coords});
+        });                                     
+    });
+});
+    
+function scrollDown(){
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+}
